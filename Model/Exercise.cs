33dd661f5d6e751a94ce142968dcdba915bc13d6
SakariasLilja <sakarias.lilja@gmail.fi>
2024-07-24 21:15:57 +0300

    Added Exercise json model

---
+++ Model/Exercise.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GymLifts.Model;
+
+public class Exercise
+{
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public string MuscleGroup { get; set; }
+}
