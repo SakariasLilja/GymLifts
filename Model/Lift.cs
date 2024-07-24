736b5a8599fff7f0add7596bb7ff7598c93a25cc
SakariasLilja <sakarias.lilja@gmail.fi>
2024-07-24 20:39:14 +0300

    Added json model for individual lifts

---
+++ Model/Lift.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GymLifts.Model;
+
+public class Lift
+{
+    public string Name { get; set; }
+    public float Weight { get; set; }
+    public int Reps { get; set; }
+    public float RPE { get; set; }
+    public string Time { get; set; }
+    public string Category { get; set; }
+    public string MuscleGroup { get; set; }
+}
