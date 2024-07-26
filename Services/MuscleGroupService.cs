d6ad39fd0be386acf8268576b1bf57fee16d8746
SakariasLilja <sakarias.lilja@gmail.fi>
2024-07-26 17:04:07 +0300

    Added service to read the muscle groups

---
+++ Services/MuscleGroupService.cs
@@ -0,0 +1,23 @@
+﻿namespace GymLifts.Services;
+
+public class MuscleGroupService
+{
+    public MuscleGroupService() { }
+
+    string[] muscleGroups = { };
+
+    public async Task<string[]> GetMuscleGroups()
+    {
+        if (muscleGroups?.Length > 0)
+            return muscleGroups;
+
+        using var stream = await FileSystem.OpenAppPackageFileAsync("musclegroups.csv");
+
+        using var reader = new StreamReader(stream);
+
+        var contents = await reader.ReadToEndAsync();
+
+        muscleGroups = contents.Split(',');
+        return muscleGroups;
+    }
+}
