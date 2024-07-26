d89c8dd2b3b4a8b7b21263d388a1e6cffb985b3d
SakariasLilja <sakarias.lilja@gmail.fi>
2024-07-26 16:54:18 +0300

    Added service to read the exercise categories

---
+++ Services/CategoryService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymLifts.Services;
+
+public class CategoryService
+{
+    public CategoryService() { }
+
+    string[] categories = { };
+
+    public async Task<string[]> GetCategories()
+    {
+        if (categories?.Length > 0)
+            return categories;
+
+        using var stream = await FileSystem.OpenAppPackageFileAsync("categories.csv");
+
+        using var reader = new StreamReader(stream);
+
+        var contents = await reader.ReadToEndAsync();
+
+        categories = contents.Split(',');
+        return categories;
+    }
+}
