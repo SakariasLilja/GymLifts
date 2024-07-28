f88bfad26f2433e3c15c16839fa8ed427b7c7fbd
SakariasLilja <sakarias.lilja@gmail.fi>
2024-07-28 22:35:42 +0300

    Added service to get exercises

---
+++ Services/ExerciseService.cs
@@ -0,0 +1,35 @@
+﻿namespace GymLifts.Services;
+
+public class ExerciseService
+{
+    public ExerciseService() { }
+
+    List<Exercise> exerciseList = new ();
+
+    public async Task<List<Exercise>> GetExercises()
+    {
+        if (exerciseList?.Count > 0)
+            return exerciseList;
+
+        using var stream = await FileSystem.OpenAppPackageFileAsync("exercises.json");
+
+        using var reader = new StreamReader(stream);
+
+        var contents = await reader.ReadToEndAsync();
+        try
+        {
+            exerciseList = JsonSerializer.Deserialize<List<Exercise>>(contents);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            Exercise exercise = new Exercise();
+            exercise.Name = "Error loading exercises";
+            exercise.MuscleGroup = "Null";
+            exercise.Category = "Null";
+            exerciseList.Add(exercise);
+        }
+
+        return exerciseList;
+    }
+}
