1b664950bba630b87e6b3e7e12c3517babd7a80a
SakariasLilja <sakarias.lilja@gmail.fi>
2024-07-30 11:53:13 +0300

    Added bindings to weight and reps

--- ViewModel/MainPageViewModel.cs
+++ ViewModel/MainPageViewModel.cs
@@ -8,8 +8,9 @@ public partial class MainPageViewModel : BaseViewModel
     ExerciseService exerciseService;
 
     public ObservableCollection<Exercise> Exercises { get; } = new ();
-
     public float[] RPEs { get; set; } = { 4, 4.5f, 5, 5.5f, 6, 6.5f, 7, 7.5f, 8, 8.5f, 9, 9.5f, 10};
+    public string Reps { get; set; } = "";
+    public string Weight { get; set; } = "";
 
     public MainPageViewModel(ExerciseService exerciseService)
     {
