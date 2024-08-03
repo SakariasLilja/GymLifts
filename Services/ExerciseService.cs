3690619b14799963049574a2de1e373025a3fa63
SakariasLilja <sakarias.lilja@gmail.fi>
2024-08-03 18:29:43 +0300

    Updated methods and private methods and added method to save exercises

--- Services/ExerciseService.cs
+++ Services/ExerciseService.cs
@@ -6,30 +6,67 @@ public class ExerciseService
 
     List<Exercise> exerciseList = new ();
 
+    private readonly string savedExercisesPath = Path.Combine(FileSystem.AppDataDirectory, "saved_exercises.json");
+
+    private async Task<List<Exercise>> GetDefaultExercises()
+    {
+        List<Exercise> output = new List<Exercise> ();
+        try
+        {
+            using var stream = await FileSystem.OpenAppPackageFileAsync("exercises.json");
+            using var reader = new StreamReader(stream);
+            var contents = await reader.ReadToEndAsync();
+            output = JsonSerializer.Deserialize<List<Exercise>>(contents);
+        }
+        catch (Exception ex) { Debug.WriteLine(ex); }
+
+        return output;
+    }
+
+    private async Task<List<Exercise>> GetSavedExercises()
+    {
+        List<Exercise> output = new List<Exercise>();
+
+        try
+        {
+            using var stream = File.OpenRead(savedExercisesPath);
+            using var reader = new StreamReader(stream);
+            var contents = await reader.ReadToEndAsync();
+            output = JsonSerializer.Deserialize<List<Exercise>>(contents);
+        }
+        catch (FileNotFoundException) { }
+        catch (Exception ex) 
+        {
+            Debug.WriteLine(ex);
+        }
+        return output;
+    }
+
     public async Task<List<Exercise>> GetExercises()
     {
         if (exerciseList?.Count > 0)
             return exerciseList;
 
-        using var stream = await FileSystem.OpenAppPackageFileAsync("exercises.json");
+        var defaultExercises = await GetDefaultExercises();
+        var savedExercises = await GetSavedExercises();
 
-        using var reader = new StreamReader(stream);
-
-        var contents = await reader.ReadToEndAsync();
-        try
-        {
-            exerciseList = JsonSerializer.Deserialize<List<Exercise>>(contents);
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine(ex);
-            Exercise exercise = new Exercise();
-            exercise.Name = "Error loading exercises";
-            exercise.MuscleGroup = "Null";
-            exercise.Category = "Null";
-            exerciseList.Add(exercise);
-        }
+        try { exerciseList = defaultExercises.Concat(savedExercises).ToList(); }
+        catch (Exception ex) { Debug.WriteLine(ex); }
 
         return exerciseList;
+    }
+
+    public async Task SaveExerciseAsync(string exerciseName, string category, string muscleGroup)
+    {
+        var savedExercises = await GetSavedExercises();
+
+        Exercise exercise = new Exercise() { Name = exerciseName, Category = category, MuscleGroup = muscleGroup };
+        savedExercises.Add(exercise);
+
+        var serializedExercises = JsonSerializer.Serialize(exerciseList);
+
+        using FileStream outputStream = File.OpenWrite(savedExercisesPath);
+        using StreamWriter writer = new StreamWriter(outputStream);
+        await writer.WriteAsync(serializedExercises);
     }
 }
