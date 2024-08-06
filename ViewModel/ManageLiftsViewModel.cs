59eed1fcd7921c68cdd0324d8639503f4753f9db
SakariasLilja <sakarias.lilja@gmail.fi>
2024-08-06 19:04:11 +0300

    Added temporary graph to design page

--- ViewModel/ManageLiftsViewModel.cs
+++ ViewModel/ManageLiftsViewModel.cs
@@ -38,4 +38,14 @@ public partial class ManageLiftsViewModel : BaseViewModel
         }
         finally { IsBusy = false; }
     }
+
+    public ISeries[] Series { get; set; }
+        = new ISeries[]
+        {
+            new LineSeries<double>
+            {
+                Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
+                Fill = null
+            }
+        };
 }
