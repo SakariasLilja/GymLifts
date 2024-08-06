2e58bfc26bcb54bcdb0c585fdf3dc1bf8a968941
SakariasLilja <sakarias.lilja@gmail.fi>
2024-08-06 18:01:02 +0300

    Added view model for managing exercises

---
+++ ViewModel/ManageLiftsViewModel.cs
@@ -0,0 +1,38 @@
+﻿using GymLifts.Services;
+
+namespace GymLifts.ViewModel;
+
+public partial class ManageLiftsViewModel : BaseViewModel
+{
+    public ObservableCollection<Exercise> Exercises { get; } = new ();
+
+    ExerciseService exerciseService;
+    public ManageLiftsViewModel(ExerciseService exerciseService) 
+    {
+        this.exerciseService = exerciseService;
+    }
+
+    [RelayCommand]
+    public async Task GetExercisesAsync()
+    {
+        if (IsBusy)
+            return;
+        
+        try
+        {
+            if (Exercises?.Count > 0)
+                return;
+
+            IsBusy = true;
+            var exercises = await exerciseService.GetExercises();
+
+            foreach (var exercise in exercises)
+                Exercises.Add(exercise);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+        }
+        finally { IsBusy = false; }
+    }
+}
