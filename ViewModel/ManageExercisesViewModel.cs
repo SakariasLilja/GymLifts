d678ea84fac5b38c06dda664b549e761aed07cd1
SakariasLilja <sakarias.lilja@gmail.fi>
2024-08-12 21:11:08 +0300

    Added methods and variables

--- ViewModel/ManageExercisesViewModel.cs
+++ ViewModel/ManageExercisesViewModel.cs
@@ -4,10 +4,41 @@ namespace GymLifts.ViewModel;
 
 public partial class ManageExercisesViewModel : BaseViewModel
 {
+    public ObservableCollection<Exercise> Exercises { get; } = new ();
+    
+    private List<Exercise> defaultExercises = new List<Exercise> ();
+
     ExerciseService exerciseService;
     public ManageExercisesViewModel(ExerciseService exerciseService)
     {
         Title = "Manage Exercises";
         this.exerciseService = exerciseService;
+    }
+
+    public async Task GetExercisesAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+
+            defaultExercises = await exerciseService.GetDefaultExercises();
+
+            var exercises = await exerciseService.GetExercises();
+
+            foreach (var exercise in exercises)
+                Exercises.Add(exercise);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Warning", "Error loading exercises", "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
     }
 }
