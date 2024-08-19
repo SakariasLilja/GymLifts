917390e8b314e095531bd31c0b1e888f2e50397d
SakariasLilja <sakarias.lilja@gmail.fi>
2024-08-19 13:39:50 +0300

    Added image file attribute

--- Model/Exercise.cs
+++ Model/Exercise.cs
@@ -7,4 +7,5 @@ public class Exercise
     public string Name { get; set; }
     public string Category { get; set; }
     public string MuscleGroup { get; set; }
+    public string ImageFile => Category.ToLower() + ".png";
 }
