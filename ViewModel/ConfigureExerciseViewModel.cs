21b09a6c0339fcdc2745aa8e3c34ea7381c235c0
SakariasLilja <sakarias.lilja@gmail.fi>
2024-08-22 20:24:05 +0300

    Created view model for exercise lift configuration

---
+++ ViewModel/ConfigureExerciseViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GymLifts.ViewModel;
+
+[QueryProperty("Exercise", "Exercise")]
+public partial class ConfigureExerciseViewModel : BaseViewModel
+{
+    public ConfigureExerciseViewModel() { }
+
+    [ObservableProperty]
+    Exercise exercise;
+
+    [ObservableProperty]
+    bool isRefreshing;
+
+}
