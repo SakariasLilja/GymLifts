cc9baa43ff26b9fa202247b5371ff6f9aba056d0
SakariasLilja <sakarias.lilja@gmail.fi>
2024-08-22 21:56:42 +0300

    Added collection for all lifts of a certain exercise and method to populate collection

--- ViewModel/ConfigureExerciseViewModel.cs
+++ ViewModel/ConfigureExerciseViewModel.cs
@@ -1,8 +1,11 @@
-﻿namespace GymLifts.ViewModel;
+﻿using GymLifts.Services;
+
+namespace GymLifts.ViewModel;
 
 [QueryProperty("Exercise", "Exercise")]
 public partial class ConfigureExerciseViewModel : BaseViewModel
 {
+    public ObservableCollection<Lift> Lifts { get; } = new();
     public ConfigureExerciseViewModel() { }
 
     [ObservableProperty]
@@ -15,6 +18,32 @@ public partial class ConfigureExerciseViewModel : BaseViewModel
     async Task GoBackButton()
     {
         await Shell.Current.GoToAsync("..", true);
+    }
+
+    public async Task GetLiftsAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            var liftService = new LiftService(this.Exercise.Name);
+
+            var lifts = await liftService.GetLiftsAsync();
+
+            Lifts.Clear();
+
+            foreach (var lift in lifts)
+                Lifts.Add(lift); 
+        }
+
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error", "Error getting lifts.", "OK");
+        }
+
+        finally { IsBusy = false; }
     }
 
 }
