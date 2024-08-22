c8da0380ca2417c93b07bce88c46fed02d0791da
SakariasLilja <sakarias.lilja@gmail.fi>
2024-08-22 21:57:03 +0300

    Made page load exercises upon load

--- View/ConfigureExercisePage.xaml.cs
+++ View/ConfigureExercisePage.xaml.cs
@@ -2,9 +2,17 @@ namespace GymLifts.View;
 
 public partial class ConfigureExercisePage : ContentPage
 {
+	ConfigureExerciseViewModel viewModel;
 	public ConfigureExercisePage(ConfigureExerciseViewModel viewModel)
 	{
 		InitializeComponent();
+		this.viewModel = viewModel;
 		BindingContext = viewModel;
 	}
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+		await viewModel.GetLiftsAsync();
+    }
 }
