336b7844672ac7ac8dae1f6ab50f6e39096c218b
SakariasLilja <sakarias.lilja@gmail.com>
2025-01-27 22:28:30 +0200

    Added database service

---
+++ Services/DatabaseService.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace GymLifts.Services;
+
+public class DatabaseService
+{
+    public const string DatabaseFilename = "GymLiftsSQLite.db3";
+    private readonly SQLiteAsyncConnection _connection;
+
+    public DatabaseService()
+    {
+        _connection = new SQLiteAsyncConnection(Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename));
+    }
+}
